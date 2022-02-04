14bc67da5f3c0948e51f7a18dfa8e384ad42f7a0
Cain Atkinson <yellowsink@protonmail.com>
2022-02-04 21:54:16 +0000

    root nspc

--- defgen/Program.fs
+++ defgen/Program.fs
@@ -11,12 +11,18 @@ let readStdIn () =
     readRec ""
 
 [<EntryPoint>]
-let main _ =
-    let raw = readStdIn ()
-    let parsedInput = JsonParser.parse raw
-    match parsedInput with
-    | Failure msg ->
-        printfn $"%s{msg}"
+let main args =
+    if args.Length <> 1 || String.IsNullOrWhiteSpace args[0] then
+        printfn "Please pass just a root name in argv"
         1
-    | Success rootNamespace ->
-        0
+    else
+        let raw = readStdIn ()
+        let parsedInput = JsonParser.parse raw
+        match parsedInput with
+        | Failure msg ->
+            printfn $"%s{msg}"
+            1
+        | Success nspcChildren ->
+            let rootNamespace = {name = args[0]; children = nspcChildren}
+            printfn $"%s{rootNamespace.name}, %i{rootNamespace.children.Length}"
+            0
