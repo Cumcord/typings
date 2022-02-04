dbf791057806db1a808fb81e165146fdfa343ef2
Cain Atkinson <yellowsink@protonmail.com>
2022-02-04 21:43:26 +0000

    this code is *much* cleaner :)

--- defgen/JsonParser.fs
+++ defgen/JsonParser.fs
@@ -3,6 +3,9 @@ module defgen.JsonParser
 open FSharp.Data
 open defgen.Types
 
+let listToNspc name list = Nmspc({name = name; children = list})
+
+/// Parses an array of [string, string] to a `Prop`
 let private parseProp (elems: JsonValue[]) =
     if elems.Length <> 2 then
         raise (FormatException("Props should only have two args in the array"))
@@ -12,39 +15,48 @@ let private parseProp (elems: JsonValue[]) =
             match elems[1] with
             | JsonValue.String s2 ->
                 { kind = parsePropKind s; typedef = s2 }
+                
             | _ -> raise (FormatException("Prop arrays must contain strings"))
+            
         | _ -> raise (FormatException("Prop arrays must contain strings"))
 
+/// Parses a list of props to either a NamespaceChild or nothing - use inside List.choose!
+let private parseListToProps nspName (elems: JsonValue[]) =
+    if elems.Length = 0 then
+        (*
+            remove things like the following (call this func inside List.choose):
+            "emptyNamespace": []
+        *)
+        None
+    else
+        match elems[0] with
+        | JsonValue.Array _ ->
+            elems
+            |> Array.toList
+            |> List.map(function
+                    | JsonValue.Array es -> Prp(parseProp es)
+                    | _ -> raise (FormatException("Prop list type was incorrect")))
+            |> listToNspc nspName
+            |> Some
+        | _ -> Some(Prp(parseProp elems))
+
+/// Recursively parses into a list of NamespaceChild (wraps recursed down in a Namespace)
 let rec private recursiveParse jsonVal =
     match jsonVal with
     | JsonValue.Record props ->
         props
         |> Array.toList
         |> List.choose (fun v ->
-            let listToNspc list = Nmspc({name = fst v; children = list})
             
             match snd v with
-            | JsonValue.Record _ -> (snd v) |> recursiveParse |> listToNspc |> Some
-            | JsonValue.Array elems ->
-                if elems.Length = 0 then
-                    None // List.choose BRRRRR
-                else
-                    match elems[0] with
-                    | JsonValue.Array _ ->
-                        elems
-                        |> Array.toList
-                        |> List.map(function
-                                | JsonValue.Array es -> Prp(parseProp es)
-                                | _ -> raise (FormatException("Prop list type was incorrect")))
-                        |> listToNspc
-                        |> Some
-                    | _ -> Some(Prp(parseProp elems))
-                    
+            | JsonValue.Record _ -> snd v |> recursiveParse |> listToNspc (fst v) |> Some
+            | JsonValue.Array elems -> parseListToProps (fst v) elems
             | _ -> raise (FormatException("Record value was not a valid type"))
             )
         
     | _ -> raise (FormatException("JSON was not a usable type, should be a namespace"))
 
+/// Parses a JSON string into a nice usable F# data structure
 let parse raw =
     let rawMaybeParsed = JsonValue.TryParse raw
     
