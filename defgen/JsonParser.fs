1192a3ae6063789b484958f1b4475290bfcc71ec
Cain Atkinson <yellowsink@protonmail.com>
2022-02-04 18:04:55 +0000

    gtg without testing fully but here

--- defgen/JsonParser.fs
+++ defgen/JsonParser.fs
@@ -3,7 +3,7 @@ module defgen.JsonParser
 open FSharp.Data
 open defgen.Types
 
-let private parseProp (elems: JsonValue[]) =
+let parseProp (elems: JsonValue[]) =
     if elems.Length <> 2 then
         raise (FormatException("Props should only have two args in the array"))
     else
@@ -15,32 +15,37 @@ let private parseProp (elems: JsonValue[]) =
             | _ -> raise (FormatException("Prop arrays must contain strings"))
         | _ -> raise (FormatException("Prop arrays must contain strings"))
 
-let rec private recursiveParse jsonVal =
-    match jsonVal with
-    | JsonValue.Record props ->
-        props
-        |> Array.toList
-        |> List.map (fun v ->
-                match snd v with
-                | JsonValue.Record _ -> recursiveParse (snd v)
-                | JsonValue.Array elems ->
-                    if elems.Length = 0 then
-                        []
-                    else
-                        match elems[0] with
-                        | JsonValue.Array _ ->
-                            elems
-                            |> Array.toList
-                            |> List.map(function
-                                    | JsonValue.Array es -> Prp(parseProp es)
-                                    | _ -> raise (FormatException("Prop list type was incorrect")))
-                        | _ -> [Prp(parseProp elems)]
-                        
-                | _ -> raise (FormatException("Record value was not a valid type"))
-                    )
-        |> List.concat
-        
-    | _ -> raise (FormatException("JSON was not a usable type, should be a namespace"))
+let rec private recursiveParseNamespace jsonVal name =
+    let children =
+        match jsonVal with
+        | JsonValue.Record props ->
+            props
+            |> Array.toList
+            |> List.map (fun v ->
+                let props = 
+                    match snd v with
+                    | JsonValue.Record _ -> [Nmspc(recursiveParseNamespace (snd v) (fst v))]
+                    | JsonValue.Array elems ->
+                        if elems.Length = 0 then
+                            []
+                        else
+                            match elems[0] with
+                            | JsonValue.Array _ ->
+                                elems
+                                |> Array.toList
+                                |> List.map(function
+                                        | JsonValue.Array es -> Prp(parseProp es)
+                                        | _ -> raise (FormatException("Prop list type was incorrect")))
+                            | _ -> [Prp(parseProp elems)]
+                    
+                    | _ -> raise (FormatException("Record value was not a valid type"))
+                
+                Nmspc({name = fst v; children = props})
+                )
+            
+        | _ -> raise (FormatException("JSON was not a usable type, should be a namespace"))
+    
+    {name = name; children = children}
 
 let parse raw =
     let rawMaybeParsed = JsonValue.TryParse raw
@@ -52,6 +57,6 @@ let parse raw =
         // they are a clean ish way to break out of a function early tho
         // so ill use that ig
         try
-            Success(recursiveParse rawParsed)
+            Success(recursiveParseNamespace rawParsed "")
         with
             | FormatException(e) -> Failure(e)
